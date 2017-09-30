0410b6259408fb1312b1574ae5fc5e3935a4aeb3
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-09-30 17:11:40 +0800

    new

---
+++ app/components/Component.jsx
@@ -0,0 +1,11 @@
+const React = require('react')
+
+class ComponetTest extends React.Component {
+    render() {
+        return (
+            <div data-test="test">Hello World!</div>
+        )
+    }
+}
+
+export default ComponetTest
---
+++ app/main.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import ComponentTest from './components/Component.jsx'
+
+ReactDom.render(
+    <ComponentTest/>,
+    document.getElementById('app')
+)
