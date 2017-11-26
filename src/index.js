650e3add1e0c76fd294385152a0f9eb6a363b9c6
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-11-26 14:08:59 +0800

    anew

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 
 import getRouter from './router/router'
 
-ReactDom.render(
-    getRouter(),
-    document.getElementById('app')
-)
+renderWithHotReload(getRouter())
+
+if (module.hot) {
+    module.hot.accept('./router/router', () => {
+        const getRouter = require('./router/router').default
+        renderWithHotReload(getRouter())
+    })
+}
+
+function renderWithHotReload(RootElement) {
+    ReactDom.render(
+        <AppContainer>
+            { RootElement }
+        </AppContainer>,
+        document.getElementById('app')
+    )
+}
 
