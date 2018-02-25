d6b0a1fd0f01e83fcc924c66e5fe26a210d62a49
xiaofuyesnew <xiaofuyesnew@qq.com>
2018-02-25 23:05:04 +0800

    更新

--- src/pages/Page/Page.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Page extends Component {
-    render() {
-        return (
-            <div>This is Page.</div>
-        )
-    }
-}
